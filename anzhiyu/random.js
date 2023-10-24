7b728380a13a3d823daf6d2b5b270a950f7dc601
Attack825 <2707138687@qq.com>
2023-10-24 18:41:32 +0800

    Site updated: 2023-10-24 18:41:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/05/19-26-35/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/05/19-26-35/","2023/10/24/18-03-16/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
